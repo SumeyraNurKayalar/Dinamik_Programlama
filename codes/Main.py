b931b2231e940c182e9cabc27c1e926ba78156e1
Sümeyra Nur Kayalar <149170501+SumeyraNurKayalar@users.noreply.github.com>
2025-04-26 15:03:26 +0300

    Update Main.py

--- codes/Main.py
+++ codes/Main.py
@@ -1,13 +1,14 @@
 def min_total_time(n, m, time, cost):
+
     dp = [[float('inf')] * m for _ in range(n)]
 
-    # İlk iş
     for j in range(m):
+
         dp[0][j] = time[0][j]
 
-    for i in range(1, n):
-        for j in range(m):
-            for k in range(m):
+    for i in range(1, n):    # n-1 kez çalışır
+        for j in range(m):     # her iş için m makine
+            for k in range(m):   # her j için tüm makinelerden geçiş kontrolü
                 dp[i][j] = min(dp[i][j], dp[i-1][k] + cost[k][j] + time[i][j])
 
     return min(dp[n-1])
